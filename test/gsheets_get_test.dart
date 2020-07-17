efae308a26db7cb34292cc3290e816b01c54173d
rovannlinhalis <rovann@gmail.com>
2020-07-17 09:27:08 -0300

    Update gsheets_get_test.dart

--- test/gsheets_get_test.dart
+++ test/gsheets_get_test.dart
@@ -1,34 +1,18 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:gsheets_get/gsheets_get.dart';
 
-void main() async {
+void main() {
   final GSheetsGet sheet = GSheetsGet(
       sheetId: "1FeiUH1ZDUvlcrn_AnFqEW62YdSvoDntBSfHfVjihjQ4",
       page: 1,
       skipRows: 0);
-
-  GSheetsResult result = await sheet.getSheet();
-  if (result.sucess) {
-    result.sheet.rows.forEach((row) {
-      StringBuffer buffer = new StringBuffer();
-      buffer.write(">");
-      if (row != null) {
-        row.cells.forEach((cell) {
-          buffer.write(cell?.text.toString() + "|");
-        });
-      }
-      print(buffer.toString());
-    });
-  } else {
-    print(result.message);
-  }
 
   test('Print Sheet', () async {
     GSheetsResult result = await sheet.getSheet();
 
     result.sheet.rows.forEach((row) {
       StringBuffer buffer = new StringBuffer();
-      buffer.write(">");
+      buffer.write("Row >");
       if (row != null) {
         row.cells.forEach((cell) {
           buffer.write(cell?.text.toString() + "|");
